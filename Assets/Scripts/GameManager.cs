6ce267c7ea01257cbf6e915ffe1ecfc7b0f53983
DavidStrife7 <51286862+DavidStrife7@users.noreply.github.com>
2022-04-22 20:12:22 +0900

    GameManagerImplemented
    Hopefully this works! I had to check other peoples' submissions to get a feel for it. I think this is one of the simplest ways to implement it...

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager GMinstance;
+    public string playerName;
+    public string saveDataName;
+    public int score;
+
+    private void Awake()
+    {
+        if (GMinstance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        GMinstance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public string SaveName;
+        public int SavedHighScore;
+    }
+
+    public void SaveNewHighScore()
+    {
+        SaveData newData = new SaveData();
+        newData.SaveName = saveDataName;
+        newData.SavedHighScore = score;
+
+        string json = JsonUtility.ToJson(newData);
+
+        File.WriteAllText(Application.persistentDataPath + "/savedata.json", json);
+    }
+
+    public void LoadBestScore()
+    {
+        string saveFilePath = Application.persistentDataPath + "/savedata.json";
+        if (File.Exists(saveFilePath))
+        {
+            string json = File.ReadAllText(saveFilePath);
+            SaveData loadData = JsonUtility.FromJson<SaveData>(json);
+            saveDataName = loadData.SaveName;
+            score = loadData.SavedHighScore;
+        }
+    }
+}
